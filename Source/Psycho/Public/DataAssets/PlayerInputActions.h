e7136431cf82382ba2ace492211a9bc62a0822a5
Akhmad <tadhuddinov@gmail.com>
2023-12-08 19:46:09 +0500

    Add APlayerCharacter using Enhanced Input System
    Add APlayerCharacter inherited from BaseCharacter
    Add functions and actions to P_PlayerController such as Move, Look, Sprint, SlowWalk, Attacks and etc.
    Add PlayerInputActions - AssetData that contains all available to the APlayerCharacter actions
    Create blueprints for APlayerCharacter, P_PlayerController, GameModeBase for test purposes
    Create InputActions

---
+++ Source/Psycho/Public/DataAssets/PlayerInputActions.h
@@ -0,0 +1,37 @@
+#pragma once
+ 
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InputAction.h"
+#include "PlayerInputActions.generated.h"
+ 
+/**
+ * Store pointer to Input Action for native binding
+*/
+UCLASS()
+class PSYCHO_API UPlayerInputActions : public UDataAsset
+{
+    GENERATED_BODY()
+ 
+public:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* MoveAction;
+ 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* LookAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* SprintAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* SlowWalkAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* LightAttackAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* HeavyAttackAction;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    UInputAction* TakePillAction;
+};
