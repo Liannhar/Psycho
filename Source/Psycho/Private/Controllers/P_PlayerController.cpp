e39d9139915652b31daac8a1150e601148eacc00
Akhmad <tadhuddinov@gmail.com>
2023-12-22 23:01:19 +0500

    Add Target System

--- Source/Psycho/Private/Controllers/P_PlayerController.cpp
+++ Source/Psycho/Private/Controllers/P_PlayerController.cpp
@@ -3,11 +3,28 @@
 
 #include "Controllers/P_PlayerController.h"
 #include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
 #include "Characters/Player/PlayerCharacter.h"
 #include "DataAssets\PlayerInputActions.h"
-#include "GameFramework/CharacterMovementComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+AP_PlayerController::AP_PlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraLockedOnOffset = FVector(0.f, 0.f, 100.f);
+
+	ChangeTargetAtMouseSpeed = 1.f;
+	BoxTraceWidth = 500.f;
+	BoxTraceLength = 1000.f;
+}
+
 
 void AP_PlayerController::BeginPlay()
 {
@@ -26,7 +43,10 @@ void AP_PlayerController::BeginPlay()
 		Subsystem->AddMappingContext(DefaultMappingContext, 0);
 	}
 
+	bIsLockedOnTarget = false;
 	PlayerCharacter->GetCharacterMovement()->MaxWalkSpeed = DefaultWalkSpeed;
+	CameraDefaultLocation = PlayerCharacter->GetCameraBoom()->GetRelativeLocation();
+	CameraLockedOnOffset += CameraDefaultLocation;
 }
 
 void AP_PlayerController::SetupInputComponent() 
@@ -53,9 +73,34 @@ void AP_PlayerController::SetupInputComponent()
 		EnhancedInputComponent->BindAction(InputActions->LightAttackAction, ETriggerEvent::Triggered, this, &AP_PlayerController::LightAttack);
 		EnhancedInputComponent->BindAction(InputActions->HeavyAttackAction, ETriggerEvent::Triggered, this, &AP_PlayerController::HeavyAttack);
 
+		// Pill
 		EnhancedInputComponent->BindAction(InputActions->TakePillAction, ETriggerEvent::Triggered, this, &AP_PlayerController::TakePill);
+		
+		// Lock On Target
+		EnhancedInputComponent->BindAction(InputActions->LockOnTargetAction, ETriggerEvent::Triggered, this, &AP_PlayerController::LockOnTarget);
+		EnhancedInputComponent->BindAction(InputActions->ChangeTargetOnAction, ETriggerEvent::Triggered, this, &AP_PlayerController::ChangeTargetOn);
 	}
 }
+
+
+void AP_PlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (bIsLockedOnTarget && LockedOnTarget)
+	{
+		FVector InterCameraBoomLocation = FMath::VInterpTo(PlayerCharacter->GetCameraBoom()->GetRelativeLocation(), CameraLockedOnOffset, DeltaSeconds, 5.f);
+		PlayerCharacter->GetCameraBoom()->SetRelativeLocation(InterCameraBoomLocation);
+
+		FRotator LookAtTarget = UKismetMathLibrary::FindLookAtRotation(PlayerCharacter->GetActorLocation(), LockedOnTarget->GetActorLocation());
+		FRotator Interpolation = FMath::RInterpTo(GetControlRotation(), LookAtTarget, DeltaSeconds, 5);
+		SetControlRotation(Interpolation/* FRotator(Interpolation.Pitch, Interpolation.Yaw, GetControlRotation().Roll) */);
+	} else {
+		FVector InterCameraBoomLocation = FMath::VInterpTo(PlayerCharacter->GetCameraBoom()->GetRelativeLocation(), CameraDefaultLocation, DeltaSeconds, 15.f);
+		PlayerCharacter->GetCameraBoom()->SetRelativeLocation(InterCameraBoomLocation);
+	}
+}
+
 
 void AP_PlayerController::Move(const FInputActionValue& Value)
 {
@@ -82,6 +127,8 @@ void AP_PlayerController::Move(const FInputActionValue& Value)
 
 void AP_PlayerController::Look(const FInputActionValue& Value)
 {
+	if (bIsLockedOnTarget) return;
+
 	// input is a Vector2D
 	FVector2D LookAxisVector = Value.Get<FVector2D>();
 	
@@ -170,4 +217,131 @@ void AP_PlayerController::TakePill(const FInputActionValue& Value)
 	// TODO: TakePill Logic
 	if(GEngine)
      GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, TEXT("Taking a Pill!"));	
+}
+
+
+void AP_PlayerController::LockOnTarget(const FInputActionValue& Value)
+{
+	if (!PlayerCharacter) return;
+
+	if (bIsLockedOnTarget)
+	{
+		bIsLockedOnTarget = false;
+		PlayerCharacter->bUseControllerRotationYaw = false;
+
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+		{
+			Subsystem->RemoveMappingContext(TargetOnMappingContext);
+		}
+
+		return;
+	}
+
+	FVector Start = PlayerCharacter->GetActorLocation();
+	FVector X = FVector(PlayerCharacter->GetFollowCamera()->GetForwardVector().X, PlayerCharacter->GetFollowCamera()->GetForwardVector().Y, 0);
+	FVector End = LockOnDistance * X + PlayerCharacter->GetActorLocation();
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(PlayerCharacter);
+	FHitResult HitResult;
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray;
+	ObjectTypesArray.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+
+	bool Hit = UKismetSystemLibrary::SphereTraceSingleForObjects(GetWorld(), Start, End, LockOnRadius, ObjectTypesArray, false, ActorsToIgnore, EDrawDebugTrace::ForDuration, HitResult, true);
+
+	if (!Hit) return;
+
+	bIsLockedOnTarget = true;
+	PlayerCharacter->bUseControllerRotationYaw = true;
+	LockedOnTarget = HitResult.GetActor();
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(TargetOnMappingContext, 0);
+	}
+}
+
+
+AActor* AP_PlayerController::FindTargetToChange(const FVector& Direction)
+{
+	// What kind of objects do we want to consider. Currently - Pawns
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray;
+	ObjectTypesArray.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+
+	// Containing hit results per Pawn we hit by trace
+	TArray<FHitResult> HitResults;
+
+	// List of actors to ignore when traicing. Currently - Player and LockedOnTarget
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(LockedOnTarget);
+	ActorsToIgnore.Add(PlayerCharacter);
+
+	// Size of the box that hits Pawns
+	FVector BoxSize = FVector();
+
+	// In BoxTraceStart pushing forward BoxTrace to not check Pawns that are behind Player
+	FVector BoxTraceStart = PlayerCharacter->GetActorLocation() + PlayerCharacter->GetActorForwardVector() * BoxTraceWidth;
+	FVector BoxTraceEnd =  BoxTraceStart + Direction * BoxTraceLength;
+
+	bool HitTrace = UKismetSystemLibrary::BoxTraceMultiForObjects(
+		GetWorld(), 
+		BoxTraceStart, 
+		BoxTraceEnd, 
+		FVector(1.f, BoxTraceWidth, 50.f), 
+		UKismetMathLibrary::MakeRotFromX(Direction), 
+		ObjectTypesArray, 
+		false, 
+		ActorsToIgnore, 
+		EDrawDebugTrace::ForDuration, 
+		HitResults, 
+		true
+		);
+
+	// Variable to determine the minimal Angle between PlayerForwardVector and Pawn
+	// Since maximum angle is 360 then in radian it will be 2𝝿 wich is 6,28319
+	float MinAngle = 10;
+	AActor* NewTarget = nullptr;
+
+	// Going through all Pawn (Enemies) that were hited to find new target
+	for (FHitResult Hit : HitResults)
+	{
+		// To normalize Pawn (Enemy) location we should firstly use GetSafeNormal funciton and then Normalize
+		FVector NormalizedEnemyLocation = (Hit.GetActor()->GetActorLocation() - PlayerCharacter->GetActorLocation()).GetSafeNormal();
+		NormalizedEnemyLocation.Normalize();
+
+		// Angle in radians between Pawn (Enemy) and Player
+		float PlayerEnemyAngle = FMath::Acos(FVector::DotProduct(PlayerCharacter->GetActorForwardVector(), NormalizedEnemyLocation));
+
+		// Drawing spheres on Pawns that were hited by BoxTrace
+		UKismetSystemLibrary::DrawDebugSphere(GetWorld(), Hit.GetActor()->GetActorLocation(), 100.f, 12, FLinearColor::White, 5, 1.f);
+
+		// Updating NewTarget and MinAngle when found lower angle between Player and Pawn
+		if (PlayerEnemyAngle < MinAngle)
+		{
+			NewTarget = Hit.GetActor();
+			MinAngle = PlayerEnemyAngle;
+		}
+	}
+
+	return NewTarget;
+}
+
+
+void AP_PlayerController::ChangeTargetOn(const FInputActionValue& Value)
+{
+	// Getting X input of a mouse to know at what direction Player is trying to change target
+	float MouseInputX = Value.Get<FVector>().X;
+
+	// Checking if Player trying to change target thats on the right. X input is positive when moving right
+	bool bRight = MouseInputX > 0;
+
+	if(GEngine)
+     GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Yellow, FString::Printf(TEXT("MouseInputX: %f"), MouseInputX));
+	
+	// if absolute value of X input is greater than speed treshhold then change target
+	if (fabs(MouseInputX) >= ChangeTargetAtMouseSpeed)
+	{
+		AActor* NewTarget = FindTargetToChange(PlayerCharacter->GetActorRightVector() * (bRight ? 1 : -1));
+		
+		LockedOnTarget = NewTarget ? NewTarget : LockedOnTarget;
+	}
 }
--- Source/Psycho/Public/Controllers/P_PlayerController.h
+++ Source/Psycho/Public/Controllers/P_PlayerController.h
@@ -19,35 +19,71 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UInputMappingContext* DefaultMappingContext;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* TargetOnMappingContext;
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	class UPlayerInputActions* InputActions;
 
+	class APlayerCharacter* PlayerCharacter;
+
+	// Movement system
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
 	float DefaultWalkSpeed;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
 	float SprintSpeed;
-
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
 	float SlowWalkSpeed;
 
 	bool bIsSprinting;
 	bool bIsSlowWalking;
 
-	class APlayerCharacter* PlayerCharacter;
+	// Target System
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
+	float LockOnDistance;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
+	float LockOnRadius;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true"))
+	FVector CameraLockedOnOffset;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true"))
+	float ChangeTargetAtMouseSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true"))
+	float BoxTraceWidth;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Target System", meta = (AllowPrivateAccess = "true"))
+	float BoxTraceLength;
+
+	AActor* LockedOnTarget;
+	bool bIsLockedOnTarget;
+	FVector CameraDefaultLocation;
 	
 protected:
-	virtual void SetupInputComponent() override;
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	virtual void Tick(float DeltaSeconds) override;
 
+	// Movement
 	void Move(const FInputActionValue& Value);
-	void Look(const FInputActionValue& Value);
 	void Sprint(const FInputActionValue& Value);
 	void StopSprint(const FInputActionValue& Value);
 	void SlowWalk(const FInputActionValue& Value);
 	void StopSlowWalk(const FInputActionValue& Value);
+	
+	// Look
+	void Look(const FInputActionValue& Value);
+
+	// Attacks
 	void LightAttack(const FInputActionValue& Value);
 	void HeavyAttack(const FInputActionValue& Value);
+
+	// Pill
 	void TakePill(const FInputActionValue& Value);
+
+	// Target System
+	void LockOnTarget(const FInputActionValue& Value);
+	void ChangeTargetOn(const FInputActionValue& Value);
+	AActor* FindTargetToChange(const FVector& Direction); 
+
+public:
+	AP_PlayerController();
 };
 
